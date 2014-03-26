5d946e81139f5c9b22f9dcfd7d0d4072c6b34953
Ryan Benoit <rdbenoit@me.com>
2014-03-26 07:59:12 -0600

    Simplification for opsworks test
    Signed-off-by: Ryan Benoit <rdbenoit@me.com>

--- vendor/cookbooks/boost/attributes/default.rb
+++
@@ -1,4 +0,0 @@
-default['boost']['source'] = "http://sourceforge.net/projects/boost/files/boost/1.51.0/"
-default['boost']['file'] = "boost_1_51_0.tar.gz"
-default['boost']['build_dir'] = "boost_1_51_0"
-default['boost']['check_for_file'] = "libboost_iostreams.so.1.51.0"

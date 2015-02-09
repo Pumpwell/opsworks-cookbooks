64ddf3786f764c4909e1a972a0f42711401681c4
Ryan Benoit <rdbenoit@me.com>
2015-02-08 19:29:12 -0700

    Added apt and mqtt-broker
    Signed-off-by: Ryan Benoit <rdbenoit@me.com>

---
+++ mqtt-broker/attributes/default.rb
@@ -0,0 +1,33 @@
+case node['platform']
+when "debian", "ubuntu"
+  default['mqtt-broker']['path'] = '/etc/mosquitto/'
+when "redhat", "centos", "scientific", "fedora", "arch", "amazon"
+  default['mqtt-broker']['path'] = '/etc/mosquitto/'
+else
+  default['mqtt-broker']['path'] = '/tmp/mosquitto'
+end
+
+default['mqtt-broker']['listeners'] = [
+  {port: 1883, addr: '127.0.0.1'},
+  {port: '8883'}
+]
+
+default['mqtt-broker']['autosave_interval'] = '1800'
+default['mqtt-broker']['allow_anonymous'] = "false"
+
+default['mqtt-broker']['connection_messages'] = "true"
+
+default['mqtt-broker']['persistence'] = "true"
+
+default['mqtt-broker']['persistence_location'] = "/tmp/"
+
+default['mqtt-broker']['persistence_file'] = "mosquitto.db"
+
+default['mqtt-broker']['persistent_client_expiration'] = "1m"
+
+default['mqtt-broker']['retained_persistence'] = "true"
+
+default['mqtt-broker']['require_certificate'] = "false"
+
+default['mqtt-broker']['bridges'] = []
+default['mqtt-broker']['listeners'] = []
---
+++ mqtt-broker/recipes/default.rb
@@ -0,0 +1,61 @@
+#
+# Cookbook Name:: mqtt-broker
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+apt_repository "mosquitto" do
+  uri "http://ppa.launchpad.net/mosquitto-dev/mosquitto-ppa/ubuntu"
+  distribution node['lsb']['codename']
+  components ["main"]
+  keyserver "keyserver.ubuntu.com"
+  key "262C4500"
+end
+
+package "mosquitto" do
+    action :install
+end
+
+cookbook_file "pumpwell.ca.bundledca.crt" do
+  backup false
+  path "/etc/mosquitto/pumpwell.ca.bundledca.crt"
+  action :create_if_missing
+end
+
+cookbook_file "pumpwell.ca.pem" do
+  backup false
+  path "/etc/mosquitto/pumpwell.ca.pem"
+  action :create_if_missing
+end
+
+cookbook_file "pumpwell.ca.crt" do
+  backup false
+  path "/etc/mosquitto/pumpwell.ca.crt"
+  action :create_if_missing
+end
+
+template "/etc/mosquitto/mosquitto.conf" do
+  source "mosquitto.conf.erb"
+
+  mode 0640
+
+  notifies :reload, "service[mosquitto]"
+end
+
+service 'mosquitto' do
+  # provider service_provider
+  service_name 'mosquitto'
+
+  supports restart: true, status: true, reload: true
+
+  action :nothing
+end
+
+include_recipe 'mqtt-broker::client'
+
+service 'mosquitto' do
+  action :start
+end
